285d83a1d4efae7550d0cf08129c19e2792a6868
timothee-oc <tmth.oc@gmail.com>
2025-07-11 16:42:40 +0200

    Fix plural of Address model in admin

--- lettings/models.py
+++ lettings/models.py
@@ -13,6 +13,9 @@ class Address(models.Model):
     def __str__(self):
         return f'{self.number} {self.street}'
 
+    class Meta:
+        verbose_name_plural = "addresses"
+
 
 class Letting(models.Model):
     title = models.CharField(max_length=256)
